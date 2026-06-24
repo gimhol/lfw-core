79340a2705b3bd25b671b8e7347675e3f4f02639
gim_z66 <gimholg1992@foxmail.com>
2026-06-24 22:23:07 +0800

    .

--- include/lfw-core/defines/ArmorEnum.hpp
+++ include/lfw-core/defines/ArmorEnum.hpp
@@ -1,9 +1,30 @@
-#ifndef LFW_CORE_DEFINES_ARMOR_ENUM_HPP
-#define LFW_CORE_DEFINES_ARMOR_ENUM_HPP
+#ifndef LFW_CORE_DEFINES_ARMORENUM_HPP
+#define LFW_CORE_DEFINES_ARMORENUM_HPP
 
-#include "ArmorType.hpp"
+#include <cstdint>
+#include <optional>
+#include <string>
+#include <string_view>
+#include <unordered_map>
 
-/// ArmorEnum — TS 中的 ArmorEnum，与 ArmorType 等价
-using ArmorEnum = ArmorType;
+#include "EnumHelper.hpp"
 
-#endif
+/* 格式: X(枚举类型, 枚举名, "字符串值", "描述", 值) */
+#define ENUM_ITEMS(X)                     \
+  X(ArmorEnum, Defend, "defend", "", = 1) \
+  X(ArmorEnum, Fall, "fall", "", = 2)     \
+  X(ArmorEnum, Times, "times", "", = 3)   \
+  X(ArmorEnum, Injury, "injury", "", = 4)
+
+enum class ArmorEnum : uint8_t
+{
+  ENUM_ITEMS(ENUM_ITEM)
+};
+GEN_ENUM_STR_MAP(ArmorTypeStringMap, ENUM_ITEMS, ArmorEnum)
+GEN_ENUM_NAME_MAP(ArmorTypeNameMap, ENUM_ITEMS, ArmorEnum)
+GEN_ENUM_DESC_MAP(ArmorTypeDescMap, ENUM_ITEMS, ArmorEnum)
+#undef ENUM_ITEMS
+DEFINE_ENUM_STR_CONVERTERS(armor_type_to_string, armor_type_from_string, ArmorEnum, ArmorTypeStringMap)
+DEFINE_ENUM_STR_CONVERTERS(armor_type_to_name, armor_type_from_name, ArmorEnum, ArmorTypeNameMap)
+DEFINE_ENUM_TO_STR(armor_type_to_desc, ArmorEnum, ArmorTypeDescMap)
+#endif // LFW_CORE_DEFINES_ARMORENUM_HPP
